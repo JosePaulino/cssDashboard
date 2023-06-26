76739e33937b12cb1dda1ef8dedf806f933261d3
José Paulino <j.paulino1@hotmail.com>
2023-06-26 13:57:30 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,41 @@
 /* flex-direction: */
-//const column = document.getElementById('felxdirection-column')
-//column document.items.classList.togle()
+function row(){
+  const column = document.querySelector("#items")
+  column.style.flexDirection = "row"
+}
 
-const column = document.querySelector("#items")
-column.style.flexDirection = "column"
+function column(){
+  const column = document.querySelector("#items")
+  column.style.flexDirection = "column"
+}
 
+function rowReverse(){
+  const column = document.querySelector("#items")
+  column.style.flexDirection = "row-reverse"
+}
 
+function columnReverse(){
+  const column = document.querySelector("#items")
+  column.style.flexDirection = "column-reverse"
+}
 
 /* justify-content: */
+function flexStart(){
+  const column = document.querySelector("#items")
+  column.style.justifyContent = "flex-start"
+}
+
+function jcCenter(){
+  const column = document.querySelector("#items")
+  column.style.justifyContent = "center"
+}
+
+function flexEnd(){
+  const column = document.querySelector("#items")
+  column.style.justifyContent = "flex-end"
+}
+
+
+
 
 /* align-items: */
