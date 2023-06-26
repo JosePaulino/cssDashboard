55c76931a35b8f66f02ae7a9392882cafc8a9c4e
José Paulino <j.paulino1@hotmail.com>
2023-06-26 13:38:51 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+/* flex-direction: */
+//const column = document.getElementById('felxdirection-column')
+//column document.items.classList.togle()
+
+const column = document.querySelector("#items")
+column.style.flexDirection = "column"
+
+
+
+/* justify-content: */
+
+/* align-items: */
